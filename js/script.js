01141cb951e96ad8b223882b4983b9f92f373e55
raja-hassan <m.hassan.raj101@gmail.com>
2023-11-08 11:34:37 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  sliderPerGroup: 3,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+document.querySelector(".swiper-button-next").addEventListener("click", function () {
+  this.style.backgroundColor = "orange";
+});
+
+document.querySelector(".swiper-button-prev").addEventListener("click", function () {
+  this.style.backgroundColor = "orange";
+});
+
+
